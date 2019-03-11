b22c74984c79c12548aec95f79f684620d5c1aaa
shroukelsayed <shrouk.passiony@gmail.com>
2019-03-11 13:09:18 +0200

    order cases

--- resources/views/partials/case/index.blade.php
+++ resources/views/partials/case/index.blade.php
@@ -69,10 +69,10 @@
                 </a>
 
                 <!-- delete the nerd (uses the destroy method DESTROY /clients/{id} -->
-                {{ Form::open(array('url' => url('cases/'.$case->id.''))) }}
+              <!--   {{ Form::open(array('url' => url('cases/'.$case->id.''))) }}
                 {{ Form::hidden('_method', 'DELETE') }}
                 {{ Form::submit('Delete', array('class' => 'btn btn-danger')) }}
-                {{ Form::close() }}
+                {{ Form::close() }} -->
 
             </td>
         </tr>
